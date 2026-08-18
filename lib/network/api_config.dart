d8e41882333f90af06ac20ceafdeacd3c378ba3d
bibintang07 <bintangnurikhsan6@gmail.com>
2026-08-18 13:06:24 +0700

    hapus cuti tk hadir

--- lib/network/api_config.dart
+++ lib/network/api_config.dart
@@ -1,6 +1,6 @@
 class api_config{
-  // static const String BASE_URL = "http://api-presensi-berkah.infinityfreeapp.com/";
-  static const String BASE_URL = "http://192.168.137.25/API_Absensi";
+  static const String BASE_URL = "http://api-presensi-berkah.infinityfreeapp.com/";
+  // static const String BASE_URL = "http://192.168.137.25/API_Absensi";
   static const String loginEndpoint = '$BASE_URL/auth/login';
   
   // Kunci untuk menyimpan token di local storage
